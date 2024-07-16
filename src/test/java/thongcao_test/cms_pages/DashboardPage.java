34004a1a9114339727572d2996de05b342c70bb3
thongcao2811 <166991236+thongcao2811@users.noreply.github.com>
2024-07-16 17:48:33 +0700

    16-07-2024

--- src/test/java/thongcao_test/cms_pages/DashboardPage.java
+++ src/test/java/thongcao_test/cms_pages/DashboardPage.java
@@ -6,9 +6,10 @@ import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import thongcao_main.UI.WebUI;
 import thongcao_main.drivers.DriverManager;
+import thongcao_main.helpers.ExcelHelper;
 
 public class DashboardPage {
-    String productName = "Gio Qua Tet";
+    String inputProductName = "Gio Qua Tet";
     private By menuDashboard = By.xpath("//div[@class='d-flex align-items-start']//span[@class='aiz-side-nav-text'][normalize-space()='Dashboard']");
     private By manageProfile = By.xpath("//div[@class='d-flex align-items-start']//span[@class='aiz-side-nav-text'][normalize-space()='Manage Profile']");
     private By homePage = By.xpath("//a[contains(text(),'Home')]");
@@ -18,6 +19,14 @@ public class DashboardPage {
     private By cancelButton = By.xpath("//button[@data-key='website-popup' and @data-value='removed']");
     private By searchTextBox = By.xpath("//input[@id='search']");
     private By firstNewProduct = By.xpath("//a[contains(text(),'Giỏ quà Tết Giáp Thìn')]");
+    private By noProductFound = By.xpath("//div[@class='search-nothing p-3 text-center fs-16']");
+    private By productName = By.xpath("//h1[contains(text(),'Giỏ quà Tết Giáp Thìn')]");
+    private By soldBy = By.xpath("//div[@class='row align-items-center']//div[@class='col-auto']");
+    private By price = By.xpath("//div[@class='opacity-50 my-2'][normalize-space()='Price:']");
+    private By quantity = By.xpath("//div[@class='row no-gutters']//div[@class='col-sm-2']");
+    private By totalPrice = By.xpath("//div[contains(text(),'Total Price:')]");
+
+
 
 
     public ManageProfilePage clickManageProfile(){
@@ -42,8 +51,22 @@ public class DashboardPage {
 
     public void searchProduct(){
         WebUI.waitForPageLoaded();
-        WebUI.setTextAndKey(searchTextBox, productName, Keys.ENTER);
-        Assert.assertTrue(DriverManager.getDriver().getCurrentUrl().contains("Gio+Qua+Tet"),"");
+        WebUI.setTextAndKey(searchTextBox, inputProductName, Keys.ENTER);
+        WebUI.waitForPageLoaded();
+        WebUI.clickElement(firstNewProduct);
+        WebUI.sleep(3);
+    }
+
+    public void setProductInfo(){
+        ExcelHelper excelHelper = new ExcelHelper();
+        excelHelper.setExcelFile("src/test/resources/testdata/ProductInfo.xlsx","Sheet1");
+        excelHelper.setCellData(WebUI.getElementText(productName),"Name",1);
+        excelHelper.setCellData(WebUI.getElementText(soldBy),"Soldby",1);
+        excelHelper.setCellData(WebUI.getElementText(price),"Price",1);
+        excelHelper.setCellData(WebUI.getElementText(quantity),"Quantity",1);
+        excelHelper.setCellData(WebUI.getElementText(totalPrice),"TotalPrice",1);
+
+
 
     }
 
--- src/test/java/thongcao_test/cms_tests/DashboardTest.java
+++ src/test/java/thongcao_test/cms_tests/DashboardTest.java
@@ -1,10 +1,28 @@
 package thongcao_test.cms_tests;
 
+import org.testng.annotations.Test;
+import thongcao_main.UI.WebUI;
+import thongcao_main.configs.ConfigData;
+import thongcao_main.helpers.ExcelHelper;
 import thongcao_test.basetest.BaseTest;
+import thongcao_test.cms_pages.DashboardPage;
 import thongcao_test.cms_pages.LoginPage;
 
 public class DashboardTest extends BaseTest {
     LoginPage loginPage = new LoginPage();
+    DashboardPage dashboardPage = new DashboardPage();
+
+
+    @Test
+    public void setProductInfoToExcelFile(){
+        dashboardPage = loginPage.loginCMS(ConfigData.EMAILCUSTOMER, ConfigData.PASSWORDCUSTOMER);
+        loginPage.verifyLoginSuccess();
+        dashboardPage.clickCancelButton();
+        dashboardPage.searchProduct();
+        dashboardPage.setProductInfo();
+
+    }
+
 
 
 }
